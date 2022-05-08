84ca55f7b1c5d524a7033cf9b358cd219522bedd
Tayna <taynasena@hotmail.com>
2022-05-08 15:58:29 -0300

    GraphQL

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -4,6 +4,10 @@ class UsersAPI extends RESTDataSource {
     constructor(){
         super()
         this.baseURL = 'http://localhost:3000'
+        this.respostaCustom = {
+            code: 200,
+            mensagem: "operação efetuada com sucesso"
+        }
     }
 
     async getUsers (){
@@ -23,6 +27,33 @@ class UsersAPI extends RESTDataSource {
         return user
     }
 
+    async adicionaUser (user){
+        const users = await this.get('/users')
+        user.id = users.length + 1
+        const role = await this.get(`roles?type=${user.role}`)
+        await this.post('users', {...user, role: role[0].id})
+        return ({
+            ...user,
+            role: role[0]
+        })
+    }
+
+    async atualizaUser (novosDados){
+        const role = await this.get(`roles?type=${novosDados.user.role}`)
+        await this.put(`users/${novosDados.id}`, {...novosDados.user, role: role[0].id})
+        return ({
+            ...this.respostaCustom,
+            user: {
+                ...novosDados.user,
+            },
+            role: role[0]
+        })
+    }
+
+    async deletaUser (id){
+        await this.delete(`users/${id}`)
+        return this.respostaCustom
+    }
 }
 
 module.exports = UsersAPI
